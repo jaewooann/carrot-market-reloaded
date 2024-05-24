77fa1d6c47798a1e408f83a29b5d112dca301a97
Jay-Ahn <111404250+jaewooann@users.noreply.github.com>
2024-05-24 17:04:04 +0900

    240524 [branch:master] 업데이트 내역
    1. 10.9 인피니티 스크롤링부터 시작.

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import ListProduct from "./list-product";
 import { InitialProducts } from "@/app/(tabs)/products/page";
+import { getMoreProducts } from "@/app/(tabs)/products/actions";
 
 interface ProductListProps {
   initialProducts: InitialProducts;
@@ -10,12 +11,36 @@ interface ProductListProps {
 
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const onLoadMoreClick = async () => {
+    setIsLoading(true);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
+    setIsLoading(false);
+  };
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button>Load more</button>
+      {isLastPage ? (
+        "No more items"
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load more"}
+        </button>
+      )}
     </div>
   );
 }
